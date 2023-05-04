da0d302089d6b8d161cf197ca22176de483b9371
Evengyl <dark.evengyl@gmail.com>
2023-05-04 16:28:31 +0200

    imp for first crud

--- src/_user/_user.controller.ts
+++ src/_user/_user.controller.ts
@@ -1,9 +1,80 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { UserService } from "./_user.service";
 
-@Controller("user")
+@Controller("api/users")
 export class UserController
 {
-    constructor(){
-        
+    constructor(
+        private readonly userServe : UserService
+    )
+    {}
+
+    //CRUD -> Create (ReadAll, ReadOne) Update Delete
+    /*
+        RestFul 
+            users/1/toutCeQueJaiBesoin NOK !!!!
+            
+            --> step by step OK !!!
+            users/1/infos/telSorte1
+                users/1/infos/telSorte2
+                    users/1/infos/telSorte3
+                        users/1/infos/telSorte4
+                            users/1/infos/telSorte5
+                                users/1/infos/telSorte6
+
+
+        GraphQL -> non vu en formation (sauf si vous êtes gentils et que le formateur aime les suchi...)
+    */
+
+
+    //  --> GET --> /api/users
+    @Get()
+    getAllUsers()
+    {
+        return this.userServe.getAll()
     }
+
+
+    // --> GET --> /api/users/:userId
+    @Get(":userId")
+    getOneUser(
+        @Param("userId") userId : number
+    )
+    {
+        return this.userServe.getOne(userId)
+    }
+
+
+    // --> POST --> /api/users
+    @Post()
+    createUser(
+        @Body() newUser : any
+    )
+    {
+        return this.userServe.create(newUser)
+    }
+
+
+    // --> PATCH --> /api/users/:userId
+    @Patch(":userId")
+    updateUser(
+        @Param("userId") userId : number,
+        @Body() updateUser : any
+    )
+    {
+        console.log(userId)
+        console.log(updateUser)
+    }
+
+
+    // --> DELETE --> /api/users/:userId
+    @Delete(":userId")
+    disableUser(
+        @Param("userId") userId : number
+    )
+    {
+        console.log(userId)
+    }
+
 }
+
--- src/_user/_user.service.ts
+++ src/_user/_user.service.ts
@@ -1,7 +1,68 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class UserService
 {
-    
+
+    private users : any[] = [
+        { id : 1, login : "Sébastien", mdp : "Test1234" },
+        { id : 2, login : "Aymeric", mdp : "Test1234" },
+        { id : 3, login : "Amandine", mdp : "Test1234" },
+        { id : 4, login : "Rémy", mdp : "Test1234" },
+        { id : 5, login : "Ferdinando", mdp : "Test1234" },
+        { id : 6, login : "Nicolas", mdp : "Test1234" },
+        { id : 7, login : "Meroine", mdp : "Test1234" }
+    ]
+
+    constructor(){
+    }
+
+
+    async getAll()
+    {
+        return this.users
+    }
+
+    async getOne(userId : number)
+    {
+        let userFoundShort = this.users.find(user => user.id == userId)
+
+        let userFoundLong = this.users.find((user) => {
+            if(user.id == userId)
+                return user
+            else
+                return undefined
+        })
+
+        //console.log(userFoundLong)
+        //console.log(userFoundShort)
+        return userFoundShort
+    }
+
+    async create(newUser : any)
+    {
+        let totalUser = this.users.length
+        let newId = totalUser+1
+
+        if(newUser.login != undefined && newUser.mdp != undefined)
+        {
+            this.users.push({id : newId, ...newUser})
+            return newId
+        }
+        else
+        {
+            throw new HttpException("Erreur : Nombre de paramètre body incorrect", HttpStatus.BAD_REQUEST)
+        }
+
+    }
+
+    async update()
+    {
+
+    }
+
+    async disable()
+    {
+
+    }
 }
